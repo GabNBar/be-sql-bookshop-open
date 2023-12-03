c4ca1faa9f5b09f96aa0d9eeaefef7d3a156a68a
gabnbar <gabitaewang@gmail.com>
2023-12-03 12:19:20 +0000

    3 insert book data into books table

--- db/02-create-books-table.sql
+++ db/02-create-books-table.sql
@@ -1,3 +1,11 @@
 \c my_bookshop;
 
 -- create the books table here
+CREATE TABLE books_table (
+    unique_identifier SERIAL PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    price_in_pence INT,
+    quantity_in_stock INT,
+    release_date DATE,
+    is_fiction BOOLEAN
+);
