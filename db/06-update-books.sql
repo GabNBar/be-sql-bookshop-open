32df343437b1bbb35aa64525c7bd3225196aaddc
gabnbar <gabitaewang@gmail.com>
2023-12-03 13:08:28 +0000

    update sale price based on quantity

--- db/06-update-books.sql
+++ db/06-update-books.sql
@@ -2,4 +2,8 @@
 
 \echo '\n These books have been put on sale, due to excess stock:\n'
 
+UPDATE books_table
+SET price_in_pence = price_in_pence * 0.9
+WHERE quantity_in_stock > 0;
 
+SELECT * FROM books_table;
